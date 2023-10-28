3c299424f3cb79ab729c0213d030ed69f6b808e8
Jonathan. S <110708850+JonathanS19@users.noreply.github.com>
2023-10-28 13:10:04 +0530

    Update LearningPython.py

--- LearningPython.py
+++ LearningPython.py
@@ -11,3 +11,23 @@ name2 = input()
 print(name2) #This line will only print the name on line 2 of the input pane.
 name3 = input("Name:")
 print(name3)
+
+num1 = int(input())
+num2 = int(input())
+mul = num1*num2
+print(mul)
+
+name = input()
+char = len(name)
+print(char)
+
+#Variables 
+
+a = input()
+b = input()
+c = 0
+c = a
+a = b
+b = c
+print("a: " + a)
+print("b: " + b)
