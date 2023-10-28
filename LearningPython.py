786d57891ba4ab541552bab3c18f3afcfd75e22b
Jonathan. S <110708850+JonathanS19@users.noreply.github.com>
2023-10-28 13:06:20 +0530

    Update and rename Learning Python to LearningPython.py

---
+++ LearningPython.py
@@ -0,0 +1,13 @@
+# Day 1
+# Basic Printing
+print('Day 1 - Python Print Function')
+print('The function is declared like this:')
+print("print('what to print')")
+
+# Basic input fuction practice
+name1 = input("What is your name? ")
+print(name1) #This line will print the text and line 1 of the input into the output.
+name2 = input()
+print(name2) #This line will only print the name on line 2 of the input pane.
+name3 = input("Name:")
+print(name3)
