64c584bd128690ef24752687338a666812e0e05c
Jonathan. S <110708850+JonathanS19@users.noreply.github.com>
2023-11-07 22:36:01 +0530

    LearningPython.py

--- LearningPython.py
+++ LearningPython.py
@@ -216,3 +216,14 @@ for i in range(0, len(student_scores)):
   if student_scores[i]>max:
     max = student_scores[i]
 print("The highest score in the class is:",max)
+
+#FizzBuzz
+for i in range(1,101):
+  if i%3==0 and i%5==0:
+    print("FizzBuzz")
+  elif i%3==0:
+    print("Fizz")
+  elif i%5==0:
+    print("Buzz")
+  else:
+    print(i)
