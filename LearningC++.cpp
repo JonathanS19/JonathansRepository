5b9d4e7f672402f54a805fc65bf0af4588159f64
Jonathan. S <110708850+JonathanS19@users.noreply.github.com>
2024-01-15 11:32:32 +0530

    Create LearningC++.cpp

---
+++ LearningC++.cpp
@@ -0,0 +1,33 @@
+1)Voter 
+#include<iostream>
+using namespace std;
+int main()
+{
+    int age;
+    cout<<"Enter your age : ";
+    cin>>age;
+
+    if(age<18)
+        cout<<"Go to kindergarden !!";
+    else
+        cout<<"You can vote !";
+}
+
+2)Multi Table
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int no;
+    int i = 1;
+    cout<<"Enter the number : ";
+    cin>>no;
+
+    while(i<=10)
+    {
+        cout<<no<<"*"<<i<<" = "<<no*i<<endl;
+        i++;
+
+    }
+}
