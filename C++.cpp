7ce921c1735b0deddea2bbe18d1a6a26925c6638
Jonathan. S <110708850+JonathanS19@users.noreply.github.com>
2024-01-15 11:17:43 +0530

    LearningC++.cpp

--- C++.cpp
+++ C++.cpp
@@ -1,1 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int age;
+    cout<<"Enter your age : ";
+    cin>>age;
 
+    if(age<18)
+        cout<<"Go to kindergarden !!";
+    else
+        cout<<"You can vote !";
+}
