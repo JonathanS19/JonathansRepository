2788b304615be2b7412166c9e1c1d381ecef0b96
Jonathan. S <110708850+JonathanS19@users.noreply.github.com>
2024-01-16 19:02:59 +0530

    Update LearningC++.cpp

--- LearningC++.cpp
+++ LearningC++.cpp
@@ -31,3 +31,50 @@ int main()
 
     }
 }
+
+3)Working with arrays 
+    #include<iostream>
+using namespace std;
+
+int main()
+{
+    int size;
+    cout<<"Enter the size of the array : ";
+    cin>>size;
+    int arr[size];
+    int i = 0;
+    int j = 0;
+    int sum = 0;
+    cout<<"Enter the array values : ";
+    while(i<size)
+    {
+        cin>>arr[i];
+        i++;
+    }
+
+    while(j<size)
+    {
+        sum = sum + arr[j];
+        j++;
+    }
+    cout<<"The sum of the arrays is : "<<sum;
+}
+4)Functions 
+#include<iostream>
+using namespace std;
+
+void sumofnum(int num1,int num2)
+{
+    int sum = 0;
+    sum = num1+num2;
+    cout<<"The sum is "<<sum;
+
+}
+int main()
+{
+    int num1,num2;
+    cout<<"Enter the values : ";
+    cin>>num1>>num2;
+    sumofnum(num1,num2);
+
+}
