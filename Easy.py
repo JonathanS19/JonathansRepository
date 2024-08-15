3db92833c02d2e76fd07f94066638c7a1fcf6904
Jonathan. S <110708850+JonathanS19@users.noreply.github.com>
2024-08-15 17:29:40 +0530

    Update Easy.py

--- Easy.py
+++ Easy.py
@@ -33,3 +33,36 @@ class Solution(object):
                 return False
         
         return True
+2)860. Lemonade Change
+class Solution:
+    def lemonadeChange(self, bills: List[int]) -> bool:
+        fives = 0
+        tens = 0
+        twenties = 0
+        sum = 0
+
+        for val in bills:
+            sum+=val
+            if val == 5:
+                fives+=1
+            elif val == 10:
+                tens+=1
+            else:
+                twenties+=1
+
+        chg = sum - (len(bills)*5)
+        print(chg,"\n")
+        ovr = 0
+
+        for i,curr in enumerate(bills):
+            ovr = curr
+            if curr == chg:
+                return True
+            for j,next in enumerate(bills[i+1:]):
+                ovr += next
+                print(ovr)
+                if next == chg:
+                    return True
+                elif ovr == chg:
+                    return True
+        return False
