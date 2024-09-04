061f25007128d3b148ecb91c26e880cb31ec3842
Jonathan. S <110708850+JonathanS19@users.noreply.github.com>
2024-09-04 11:13:00 +0530

    Update Medium.py

--- Medium.py
+++ Medium.py
@@ -7,3 +7,37 @@ class Solution:
                     return i
                 else:
                     k-=val
+2)7. Reverse Integer
+class Solution:
+    def reverse(self, x: int) -> int:
+        lst = list(str(x))
+        lst = lst[::-1]
+        new = []
+
+        if '-' in lst:
+            new.append('-')
+            for i,val in enumerate(lst):
+                if lst[0] == '0' or val == '-':
+                    continue
+                else:
+                    new.append(val)
+        
+        else:
+            for i,val in enumerate(lst):
+                if i == 0 and val == '0':
+                    continue
+                else:
+                    new.append(val)
+
+        res = ""
+        for val in new:
+            res+=val
+        if res != "":
+            res = int(res)
+        else:
+            return 0
+
+        if res < -2**31 or res > 2**31 - 1 or res == 0:
+            return 0
+        else:
+            return res
